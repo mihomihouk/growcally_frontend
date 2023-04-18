85cea7ca29d53052ac06baa3db86c9bd5429336f
Miho Inagaki <mihoinagaki@mihonomacbookea.home>
2023-04-18 13:12:02 +0100

    install fvilers

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import App from "./App";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
