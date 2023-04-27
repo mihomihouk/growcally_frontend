74b8a6f70c22f01d0606e3615bc8bbd7be1a87d1
Miho Inagaki <mihoinagaki@mihonomacbookea.home>
2023-04-27 11:15:39 +0100

    feat(auth): Create login page

--- src/pages/log-in-page.tsx
+++ src/pages/log-in-page.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Button } from '../components/button';
+import { Link } from 'react-router-dom';
+import { SIGN_UP_PATH } from '../routes';
 
-export const SignUpPage = () => {
+export const LogInPage = () => {
   return (
     <div className="min-h-screen py-40 bg-gray-300">
       <div className="container mx-auto">
         <div className="flex flex-col w-10/12 lg:w-8/12 bg-white rounded-xl shadow-lg overflow-hidden mx-auto">
           <div className="flex flex-col lg:flex-row ">
-            <div className="w-full lg:w-1/2">
+            <div className="w-full lg:w-1/2 ">
               <div className="flex items-center gap-4 pl-4 pt-4">
                 <img
                   alt="growcally logo"
@@ -16,23 +18,11 @@ export const SignUpPage = () => {
                 />
               </div>
               <div className="py-16 px-12">
-                <h2 className="text-3xl mb-4">Welcome</h2>
+                <h2 className="text-3xl mb-4">Welcome back</h2>
                 <p className="mb-4 text-gray-400">
-                  Create your account. It’s free and only take a minute.
+                  Welcome back! Please enter your details.
                 </p>
                 <form>
-                  <div className="grid grid-cols-2 gap-5">
-                    <input
-                      type="text"
-                      placeholder="Firstname"
-                      className="border border-gray-400 px-2 rounded-lg p-2"
-                    />
-                    <input
-                      type="text"
-                      placeholder="Surname"
-                      className="border border-gray-400 px-2 rounded-lg p-2"
-                    />
-                  </div>
                   <div className="mt-5">
                     <input
                       type="text"
@@ -47,18 +37,21 @@ export const SignUpPage = () => {
                       className="border border-gray-400 px-2 w-full rounded-lg p-2"
                     />
                   </div>
-                  <div className="mt-5">
-                    <input
-                      type="password"
-                      placeholder="Confirm Password"
-                      className="border border-gray-400 px-2 w-full rounded-lg p-2"
-                    />
-                  </div>
+
                   <div className="mt-5">
                     <Button className="w-full bg-black text-white inline-block p-2 rounded-lg hover:bg-opacity-75">
-                      Register Now
+                      Log In
                     </Button>
                   </div>
+                  <p className="text-sm text-gray-400 mt-5 text-center">
+                    Don't have an account?
+                    <Link
+                      className="ml-2 text-black font-semibold cursor-pointer"
+                      to={SIGN_UP_PATH}
+                    >
+                      Sign up
+                    </Link>
+                  </p>
                 </form>
               </div>
             </div>
