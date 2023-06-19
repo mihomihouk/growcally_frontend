a080eb090d929d9124f60bd97da43c64f4b92518
Miho Inagaki <mihoinagaki@mihonomacbookea.home>
2023-06-19 20:27:44 +0100

    Enhancement: Add test for textarea and thumbnail

---
+++ src/components/textarea.test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../util/test';
+import { TextArea } from './textarea';
+import '@testing-library/jest-dom';
+
+const mockData = {
+  text: 'We planted cabbage this morning!',
+  errorMessage: 'There is an error'
+};
+
+describe('textArea component', () => {
+  it('should render value', () => {
+    renderWithProviders(<TextArea defaultValue={mockData.text} />);
+    expect(screen.getByText(mockData.text)).toBeInTheDocument();
+  });
+
+  it('should render error styles when there is an error', () => {
+    renderWithProviders(
+      <TextArea
+        defaultValue={mockData.text}
+        hasError
+        errorMessage={mockData.errorMessage}
+      />
+    );
+    const textareaEl = screen.getByRole('textbox');
+    expect(textareaEl).toHaveClass('border-error-500');
+    expect(screen.getByText(mockData.text)).toBeInTheDocument();
+  });
+});
---
+++ src/components/thumbnail.test.tsx
@@ -0,0 +1,18 @@
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../util/test';
+import { Thumbnail } from './thumbnail';
+import '@testing-library/jest-dom';
+
+const mockData = {
+  src: '/img/default-profile.jpg',
+  alt: 'profile picture'
+};
+
+describe('Thumbnail component', () => {
+  it.only('should render image properly', () => {
+    renderWithProviders(<Thumbnail src={mockData.src} alt={mockData.alt} />);
+    const imageEl = screen.getByAltText(mockData.alt);
+    expect(imageEl).toBeInTheDocument();
+    expect(screen.getByRole('img')).toHaveAttribute('src', mockData.src);
+  });
+});
